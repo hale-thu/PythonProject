8024f51dfab67978a03bd3c4e1f171912db38c57
halei96 <hale190596@gmail.com>
2026-09-22 00:22:11 +0700

    Initial commit

---
+++ week1/task2.py
@@ -0,0 +1,11 @@
+# Declare and initialize variables
+zahl = 10
+kommazahl = 10.5
+text = "Hello, World!"
+wahrheitswert = True
+
+# Check the type of each variable
+print("zahl:", zahl, "- Type:", type(zahl))
+print("kommazahl:", kommazahl, "- Type:", type(kommazahl))
+print("text:", text, "- Type:", type(text))
+print("wahrheitswert:", wahrheitswert, "- Type:", type(wahrheitswert))
---
+++ week1/task3.py
@@ -0,0 +1,24 @@
+# Convert an integer to a floating-point number
+i = 5
+f = float(i)
+print("Integer to float:", f, "- Type:", type(f))
+
+# Convert a floating-point number to an integer
+f2 = 7.9
+i2 = int(f2)
+print("Float to integer:", i2, "- Type:", type(i2))
+
+# Convert an integer to a string
+i3 = 42
+s = str(i3)
+print("Integer to string:", s, "- Type:", type(s))
+
+# Convert a string containing a number to an integer
+s2 = "123"
+i4 = int(s2)
+print("String to integer:", i4, "- Type:", type(i4))
+
+# Convert an integer to a boolean
+i5 = 0
+b = bool(i5)
+print("Integer to boolean:", b, "- Type:", type(b))
